c1aa8e4e871b1783d3c027fb4a066f026fd230a8
Documenter.jl <documenter@juliadocs.github.io>
2023-04-27 06:23:36 +0000

    build based on a447e07

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ReduceWindows","category":"page"},{"location":"#ReduceWindows","page":"Home","title":"ReduceWindows","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ReduceWindows.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ReduceWindows]","category":"page"}]
+}
