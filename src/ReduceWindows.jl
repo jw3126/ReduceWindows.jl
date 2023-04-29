a486e0b57444dd72ee68316e1d63a7146e77ec57
Jan Weidner <jw3126@gmail.com>
2023-04-29 22:18:13 +0200

    improve docstring

--- src/ReduceWindows.jl
+++ src/ReduceWindows.jl
@@ -220,10 +220,16 @@ end
 
     reduce_window(f, arr, window)
 
-Move a sliding window over the `arr` and apply `reduce(f, view(arr, window...))`
-This function assumes, that `f` is associative, commutative.
+Move a sliding window over the `arr` and apply `reduce(f, view(arr, shifted window...))`
+For instance `window = (-1:2, 3:4)` will produce an output matrix with entries:
 
-Time complexity is `O(length(arr) * log(prod(length,window)))`.
+`out[i,j] = reduce(f, arr[(i-1):(i+2), (j+3):(j+4)])`
+
+This is equation is true semantically, but in implementation much less work will be done.
+Time complexity is `O(log(k) * n)` where 
+* `n` is the size of the array: `n = length(arr)`
+* `k` is the size of the window: `k = prod(length, window)`
+Note `reduce_window` assumes, that `f` is associative and commutative.
 """
 function reduce_window(f::F, arr, window) where {F}
     win = resolve_window(axes(arr), window)
